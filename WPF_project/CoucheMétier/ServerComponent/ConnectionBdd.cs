914c022fde3cfdf6eb0df8f1577344fb0c8e82c5
Patrick DELAPORTE <delaportepatrick@ymail.com>
2019-06-18 14:37:52 +0200

    routing issue

--- WPF_project/CoucheMétier/ServerComponent/ConnectionBdd.cs
+++ WPF_project/CoucheMétier/ServerComponent/ConnectionBdd.cs
@@ -1,19 +1,21 @@
 ﻿using Npgsql;
 using System;
 using System.Windows;
+using WPF_project.CoucheMétier.ServerComponent;
 
 namespace WPF_project
 {
 
     public class ConnectionBdd
     {
-        private NpgsqlConnection connection;
-        string connectionString = String.Format("Server={0};Port={1};User Id ={2};Password={3};Database={4}",
+        public static string connectionString = String.Format("Server={0};Port={1};User Id ={2};Password={3};Database={4}",
             "localhost", 5432, "postgres", "test", "ways");
+        public static NpgsqlConnection connection = new NpgsqlConnection(connectionString);
+        public UserServerComponent userService = new UserServerComponent(connection);
 
         public void ConnectToDataBase()
         {
-            connection = new NpgsqlConnection(connectionString);
+            
             try
             {
                 connection.Open();
