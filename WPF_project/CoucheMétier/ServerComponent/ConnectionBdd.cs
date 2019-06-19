1888e06283dc970cdab79253f87a254ac3c02f41
Patrick DELAPORTE <delaportepatrick@ymail.com>
2019-06-19 10:01:34 +0200

    fixed pgsql call issues

--- WPF_project/CoucheMétier/ServerComponent/ConnectionBdd.cs
+++ WPF_project/CoucheMétier/ServerComponent/ConnectionBdd.cs
@@ -9,25 +9,8 @@ namespace WPF_project
     public class ConnectionBdd
     {
         public static string connectionString = String.Format("Server={0};Port={1};User Id ={2};Password={3};Database={4}",
-            "localhost", 5432, "postgres", "test", "ways");
+            "localhost", 5432, "postgres", "1004", "ways");
         public static NpgsqlConnection connection = new NpgsqlConnection(connectionString);
         public UserServerComponent userService = new UserServerComponent(connection);
-
-        public void ConnectToDataBase()
-        {
-            
-            try
-            {
-                connection.Open();
-                MessageBox.Show("connection ok");
-                connection.Close();
-            }
-            catch
-            {
-                MessageBox.Show("erreur de connection à la base de données");
-            }
-
-        }
-
     }
 }
